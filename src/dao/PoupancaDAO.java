f05969ec7207b65360ded690eaf2fcbdba521e2b
Diogo_DC <diogodinizcosme@gmail.com>
2017-11-20 20:05:22 -0200

    Update PoupancaDAO.java

--- src/dao/PoupancaDAO.java
+++ src/dao/PoupancaDAO.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package dao;
 
 import java.sql.PreparedStatement;
@@ -17,10 +13,7 @@ import modelo.PoupancaEmBanco;
 import utilitarios.ConectaBanco;
 import utilitarios.DataHora;
 
-/**
- *
- * @author Felipe
- */
+
 public class PoupancaDAO {
     ConectaBanco conexao = new ConectaBanco();
     DataHora dth = new DataHora();
